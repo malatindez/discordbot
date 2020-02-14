08685fff10e1cb8d0581112e148c11e0afb07858
malatindez <axenance@gmail.com>
2020-02-14 16:52:17 +0200

    db fixes

--- discordbot/DataBase.py
+++ discordbot/DataBase.py
@@ -15,6 +15,7 @@ class DataBase():
  
     def commit(self):
         self.db.commit()
+
     def addGlobalUserValues(self, name, valuesList):
         if valuesList is None: 
             return
@@ -23,6 +24,7 @@ class DataBase():
                 self.cursor.execute("ALTER TABLE Users ADD COLUMN '" + name +
                                    valuesList[i][0] + "' " + valuesList[i][1])
             except sqlite3.OperationalError: pass;
+        self.db.commit()
 
     def addLocalUserValues(self, name, valuesList):
         if valuesList is None: 
@@ -32,6 +34,7 @@ class DataBase():
                 self.cursor.execute("ALTER TABLE Guilds ADD COLUMN '" + name +
                                    valuesList[i][0] + "' " + valuesList[i][1])
             except sqlite3.OperationalError: pass;
+        self.db.commit()
 
     def addGuildValues(self, name, valuesList):
         if valuesList is None: 
@@ -41,12 +44,14 @@ class DataBase():
                 self.cursor.execute("ALTER TABLE Guilds ADD COLUMN '" + name +
                                    valuesList[i][0] + "' " + valuesList[i][1])
             except sqlite3.OperationalError: pass;
+        self.db.commit()
 
     def addGuild(self, id):
         a = len(self.cursor.execute("PRAGMA table_info('Guilds')").fetchall())
         s = "INSERT INTO Guilds VALUES(" + str(id) + ", '{}', '{}' "
         for i in range(a - 3): s += ", 0"
         self.cursor.execute(s + ")")
+        self.db.commit()
 
     def addChannelLanguage(self, guildID, channelID, language):
         self.cursor.execute("SELECT channelLanguages FROM Guilds WHERE id = " + str(guildID))
@@ -55,11 +60,13 @@ class DataBase():
         v[str(channelID)] = language
         self.cursor.execute("UPDATE Guilds SET channelLanguages = '" + json.dumps(v) +
                            "' WHERE id = " + str(guildID))
+        self.db.commit()
     
     def getChannelLanguage(self, guildID, channelID):
         self.cursor.execute("SELECT channelLanguages FROM Guilds WHERE id = " + str(guildID))
         x = self.cursor.fetchall()[0][0]
         v = json.loads(x)
+        self.db.commit()
         return v[str(channelID)]
 
 
@@ -68,12 +75,13 @@ class DataBase():
         self.cursor.execute("SELECT enabledPlugins FROM Guilds WHERE id = " + str(guildID))
         x = self.cursor.fetchall()[0][0]
         v = json.loads(x)
-        if channelID not in v:
-            v[channelID] = []
-        if plugin not in v[channelID]:
-            v[channelID].append(plugin)
+        if str(channelID) not in v:
+            v[str(channelID)] = []
+        if plugin not in v[str(channelID)]:
+            v[str(channelID)].append(plugin)
         self.cursor.execute("UPDATE Guilds SET enabledPlugins = '" + json.dumps(v) +
                            "' WHERE id = " + str(guildID))
+        self.db.commit()
     
     def remEnabledPlugin(self, guildID, channelID, plugin):
         self.cursor.execute("SELECT enabledPlugins FROM Guilds WHERE id = " + str(guildID))
@@ -82,17 +90,20 @@ class DataBase():
         v[str(channelID)].remove(plugin)
         self.cursor.execute("UPDATE Guilds SET enabledPlugins = '" + json.dumps(v) +
                            "' WHERE id = " + str(guildID))
+        self.db.commit()
 
     def getEnabledPlugins(self, guildID, channelID):
         self.cursor.execute("SELECT enabledPlugins FROM Guilds WHERE id = " + str(guildID))
         x = self.cursor.fetchall()[0][0]
         v = json.loads(x)
+        self.db.commit()
         return v[str(channelID)]
 
 
 
     def getGlobalUserData(self, name, dataName, userID):
         self.cursor.execute("SELECT " + name + dataName + " FROM Users WHERE id = " + str(userID))
+        self.db.commit()
         return self.cursor.fetchall()[0][0]
 
     def writeGlobalUserData(self, name, dataName, userID, newValue):
@@ -104,25 +115,30 @@ class DataBase():
         self.cursor.execute(s)
         self.cursor.execute("UPDATE Users SET " + name + dataName + " = \"" +
                            str(newValue) + "\" WHERE id = " + str(userID))
+        self.db.commit()
 
     def getLocalUserData(self, name, dataName, guildID, userID):
         self.cursor.execute("SELECT " + name + dataName + " FROM Guilds WHERE id = " + str(guildID))
         x = self.cursor.fetchall()[0][0]
         v = json.loads(x)
+        self.db.commit()
         return v[str(userID)]
 
     def writeLocalUserData(self, name, dataName, guildID, userID, newValue):
         self.cursor.execute("SELECT " + name + dataName + " FROM Guilds WHERE id = " + str(guildID))
         x = self.cursor.fetchall()[0][0]
         v = json.loads(x)
-        v[userID] = newValue
+        v[str(userID)] = newValue
         self.cursor.execute("UPDATE Guilds SET" + name + dataName + " = \"" + 
                             str(json.dumps(v)) + "\" WHERE id = " + str(guildID))
+        self.db.commit()
 
     def getGuildData(self, name, dataName, guildID):
         self.cursor.execute("SELECT " + name + dataName + " FROM Guilds WHERE id = " + str(guildID))
         x =  self.cursor.fetchall()
+        self.db.commit()
         return x[0][0]
 
     def writeGuildData(self, name, dataName, guildID, value):
         self.cursor.execute("UPDATE Guilds SET " + name + dataName + " = '" + str(value) + "' WHERE id = " + str(guildID))
+        self.db.commit()
