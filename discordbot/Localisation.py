37c1a0529704ef374ae23d72c37466452ae844b2
malatindez <axenance@gmail.com>
2020-04-20 02:03:36 +0300

    Update Localisation.py

--- discordbot/Localisation.py
+++ discordbot/Localisation.py
@@ -1,6 +1,6 @@
 import json
 class Localisation:
     def __init__(self, path):
-        self.data = json.loads(open(path, 'r').read())
+        self.data = json.loads(open(path, 'r',encoding='utf-8').read())
     def getText(self, value, language):
         return self.data[language][value]
